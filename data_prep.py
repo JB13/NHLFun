f974ae5ae5a5a53654866fe810f6ce1984fc8393
JB13 <JB13@users.noreply.github.com>
2024-04-26 10:58:22 -0700

    Model 2 First Draft
    Started to implement the first upgrades for model 2.

--- data_prep.py
+++ data_prep.py
@@ -2,6 +2,9 @@ import pandas as pd
 import numpy as np
 
 import torch
+from torch import nn
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
 
 
 """
@@ -31,12 +34,17 @@ def create_input_data_from_csv(path_to_csv):
 
     print(goal_shot_rows["Event"])
 
+    print(list(goal_shot_rows.columns.values).count("prev_xC"))
+
     # shift the dataframe by 1 row
     shifted_df = df.shift(1)
     shifted_df.columns = ['prev_' + name for name in df.columns]
 
+    print(list(shifted_df.columns.values).count("prev_xC"))
+
     # combine goal_shot_rows with shifted_df, but using rows from goal_shot_rows
     goal_shot_rows = pd.concat([goal_shot_rows, shifted_df.loc[goal_shot_rows.index]], axis=1)
+    print(list(goal_shot_rows.columns.values).count("prev_xC"))
 
     goal_shot_rows = goal_shot_rows[goal_shot_rows['Period'] != 0]
     goal_shot_rows = goal_shot_rows[goal_shot_rows['Period'] != 5]
@@ -46,7 +54,7 @@ def create_input_data_from_csv(path_to_csv):
     val_result_data = val_result_data.apply(lambda x: 1 if x == "GOAL" else 0)
 
     val_input_data = val_input_data.drop(columns=['Event'])
-
+    print(list(val_input_data.columns.values).count("prev_xC"))
 
     # Split out the data between numeric values (can carry forward) and categorical values (need to be turned into binary columns)
     val_input_data_numeric = val_input_data[['Period', 'Seconds_Elapsed', 'xC', 'yC', 'prev_Seconds_Elapsed', 'prev_xC', 'prev_yC']]
@@ -58,21 +66,19 @@ def create_input_data_from_csv(path_to_csv):
 
     for column in ['prev_Event', 'prev_Strength', 'prev_Type']:
         dummy_columns = pd.get_dummies(val_input_data[column])
-        
+
         # rename all columns with a prefix of "prev_"
         dummy_columns.columns = ['prev_' + str(col) for col in dummy_columns.columns]
-
         val_input_data_categorical = pd.concat([val_input_data_categorical, dummy_columns], axis=1)
         
-        # change all True/False to 1/0
-        val_input_data_categorical = val_input_data_categorical.applymap(lambda x: 1 if x == True else 0)
+    # change all True/False to 1/0
+    val_input_data_categorical = val_input_data_categorical.applymap(lambda x: 1 if x == True else 0)
 
-        # combine the two dataframes
-        val_input_data_combined = pd.concat([val_input_data_numeric, val_input_data_categorical], axis=1)
+    # combine the two dataframes
+    val_input_data_combined = pd.concat([val_input_data_numeric, val_input_data_categorical], axis=1)
 
-        #set val_input_data2 to be all floats
-        val_input_data_combined = val_input_data_combined.astype(float)
-
+    #set val_input_data2 to be all floats
+    val_input_data_combined = val_input_data_combined.astype(float)
 
     columns = ['Period', 'Seconds_Elapsed', 'xC', 'yC', 'prev_Seconds_Elapsed',
             'prev_xC', 'prev_yC', 'BLOCK', 'CHL', 'DELPEN', 'FAC', 'GIVE', 'HIT',
@@ -93,10 +99,21 @@ def create_input_data_from_csv(path_to_csv):
             'prev_PS-Tripping on breakaway(0 min)', 'prev_SLAP SHOT',
             'prev_SNAP SHOT', 'prev_TIP-IN', 'prev_WRAP-AROUND', 'prev_WRIST SHOT']
 
+    print(val_input_data_combined.shape)
+    print(val_input_data_combined.columns)
     # For each column in input_data_combined, check if it exists in val_input_data_combined, if not add it as all 0s
     for column in columns:
         if column not in val_input_data_combined.columns:
                 val_input_data_combined[column] = 0
+
+    for column in val_input_data_combined.columns:
+        if column not in columns:
+                if column in val_input_data_combined.columns:
+                    print(f"Column {column} not in columns")
+                    val_input_data_combined = val_input_data_combined.drop(column, axis=1)
+
+    print(val_input_data_combined.shape)
+    print(val_input_data_combined.columns)
 
     # reduce val_input_data_combined to have the same columns as input_data_combined
     val_input_data_combined = val_input_data_combined[columns]
@@ -104,7 +121,30 @@ def create_input_data_from_csv(path_to_csv):
     # change all NaN numbers to 0
     val_input_data_combined = val_input_data_combined.fillna(0)
 
-    return val_input_data_combined, val_result_data
+    print(val_input_data_combined.shape)
+
+    filepath = '0422Model_800k.pt'
+    model = nn.Sequential(
+        nn.Linear(72, 120),
+        nn.ReLU(),
+        nn.Linear(120, 60),
+        nn.ReLU(),
+        nn.Linear(60, 1),
+        nn.Sigmoid()
+    )
+
+    model.load_state_dict(torch.load(filepath))
+    model.eval()
+    model.cpu()
+
+    x = torch.tensor(val_input_data_combined.values, dtype=torch.float32)
+    y = torch.tensor(val_result_data.values, dtype=torch.float32)
+    result = model(x)
+
+    # add the probability of a goal to val_input_data_combined
+    goal_shot_rows['Goal_Probability'] = result.detach().numpy()
+
+    return val_input_data_combined, val_result_data, goal_shot_rows
 
 
 if __name__ == "__main__":
